0c15955d65784fe52c41fc679ee4da6f9b5d9e3a
adit2810 <adit20061028@gmail.com>
2024-08-01 23:31:19 +0700

    fix : ProfileController

--- app/Http/Controllers/Api/App/ProfileController.php
+++ app/Http/Controllers/Api/App/ProfileController.php
@@ -14,13 +14,15 @@ class ProfileController extends Controller
     {
         $profiles = User::with(['employee' => function ($query) {
             $query->select('id', 'id_user', 'id_role', 'id_employee_group', 'id_position');
-        }])->get(['id', 'name', 'gender_status', 'religion']);
+        }])->get(['id', 'first_name', 'last_name', 'gender_status', 'religion', 'profile_photo']);
 
         $profiles = $profiles->map(function ($user) {
             return [
-                'name' => $user->name,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
                 'gender' => $user->gender_status,
-                'religion' => $user->religion,
+                'religion' => $user->reliigion,
+                'profile_photo' => $user->profile_photo,
                 'role' => $user->employee ? $user->employee->id_role : null,
                 'employee_group' => $user->employee ? $user->employee->id_employee_group : null,
                 'position' => $user->employee ? $user->employee->id_position : null,
@@ -34,19 +36,22 @@ class ProfileController extends Controller
     {
         $user = User::with(['employee' => function ($query) {
             $query->select('id', 'id_user', 'id_role', 'id_employee_group', 'id_position','nip');
-        }])->find($id, ['id', 'name', 'gender_status', 'religion','phone_number', 'address', 'email']);
+        }])->find($id, ['id', 'first_name', 'last_name', 'gender_status', 'religion','phone_number', 
+        'address', 'email', 'profile_photo']);
 
         if (!$user) {
             return response()->json(['message' => 'Profile not found'], 404);
         }
 
         $profile = [
-            'name' => $user->name,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
             'gender' => $user->gender_status,
             'religion' => $user->religion,
             'phone_number' => $user->phone_number,
             'address' => $user->address,
             'email' => $user->email,
+            'profile_photo' => $user->profile_photo,
             'role' => $user->employee->id_role,
             'employee_group' =>$user->employee->id_employee_group,
             'position' =>$user->employee->id_position,
