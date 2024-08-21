0e1918d0edb01ad0e6aeb64009ab12c54098e4f9
adit2810 <adit20061028@gmail.com>
2024-08-21 13:30:56 +0700

    fix : Leave Category Table

--- database/migrations/2024_07_15_010000_create_leave_category.php
+++ database/migrations/2024_07_15_010000_create_leave_category.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMasterCategoryTable extends Migration
+class CreateLeaveCategory extends Migration
 {
     public function up()
     {
