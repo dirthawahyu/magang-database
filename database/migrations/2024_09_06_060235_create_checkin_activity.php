464f0f324e1f88f545ade5967cd347646f4940ee
adit2810 <adit20061028@gmail.com>
2024-09-09 11:25:37 +0700

    refactor: Timezone Jakarta

--- database/migrations/2024_09_06_060235_create_checkin_activity.php
+++ database/migrations/2024_09_06_060235_create_checkin_activity.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -24,8 +25,10 @@ return new class extends Migration
         });
 
         DB::table('checkin_activity')->insert([
-            ['id_user' => 1, 'time' => '2024-08-10-07-24', 'type' => 0 , 'latitude' => -7.340671, 'longtitude' => 112.7364161,],
-            ['id_user' => 1, 'time' => '2024-08-10-17-00', 'type' => 1 , 'latitude' => -7.340671, 'longtitude' => 112.7364161,],
+            ['id_user' => 1, 'time' => '2024-08-10-07-24', 'type' => 0, 'latitude' => -7.340671, 'longtitude' => 112.7364161,
+            ],
+            ['id_user' => 1, 'time' => '2024-08-10-17-00', 'type' => 1, 'latitude' => -7.340671, 'longtitude' => 112.7364161,
+            ],
         ]);
     }
 
